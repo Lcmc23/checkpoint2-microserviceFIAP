cd12071c646cf868fb3ec40803c827437d11b655
Lucas Cabral Mendes Corrêa <lcmc23@MacBook-Air-de-Lucas.local>
2024-05-03 23:37:04 -0300

    Checkpoint2-pt3_ArquivoCompleto

---
+++ Checkpoint2-pt3/src/app/components/fornecedor-detail/fornecedor-detail.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FornecedorService } from '../../services/fornecedor.service';
+import { Fornecedor } from '../../interfaces/fornecedor';
+import { FormGroup, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-fornecedor-detail',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './fornecedor-detail.component.html',
+  styleUrl: './fornecedor-detail.component.css'
+})
+export class FornecedorDetailComponent {
+
+  fornecedor?: Fornecedor;
+
+  fornecedorForm: FormGroup = new FormGroup({})
+
+  constructor(private route: ActivatedRoute, private fornecedorService: FornecedorService, private formbuilder: FormBuilder) {
+    this.getFornecedorById()
+  }
+
+  id?: string;
+  getFornecedorById() {
+    this.id = this.route.snapshot.paramMap.get('id') ?? ''
+
+    this.fornecedorService.getById(this.id).subscribe((fornecedorResponse) => this.fornecedor = fornecedorResponse)
+
+    this.fornecedorForm = this.formbuilder.group({
+      nome: [this.fornecedor?.nome],
+      endereco: [this.fornecedor?.endereco],
+      telefone: [this.fornecedor?.telefone],
+      id: [this.fornecedor?.id]
+    })
+
+  }
+
+  update(): void {
+    if (this.fornecedorForm.valid) {
+      const fornecedorAlterado: Fornecedor = {
+        nome: this.fornecedorForm.value.nome,
+        endereco: this.fornecedorForm.value.endereco,
+        telefone: this.fornecedorForm.value.telefone,
+        id: this.fornecedorForm.value.id
+      }
+
+      this.fornecedorService.atualizar(fornecedorAlterado).subscribe()
+      alert('Alterado com sucesso!')
+    }
+  }
+
+}
+
